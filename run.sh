260104f4d7d9fdaa1ccb54e64ec37d2317f20c0f
sroabo <sro.abo@gmail.com>
2025-11-26 13:45:48 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/env bash
-export EVOHOME2MQTT_USER="${evohome_user}"
-export EVOHOME2MQTT_PASSWORD="${evohome_password}"
-export EVOHOME2MQTT_MQTT="mqtt://${mqtt_user}:${mqtt_password}@${mqtt_host}:${mqtt_port}"
-export EVOHOME2MQTT_POLLING_INTERVAL="${polling_interval}"
-export EVOHOME2MQTT_RUNNING_IN_CONTAINER=true
-exec node ./lib/bridge.js
-
